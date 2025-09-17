50dae88229eca8c66e3dc3c8aa16b465d4248f86
Ben Montimurro <benmonti@udel.edu>
2025-09-17 00:06:55 -0400

    worked on task 7

--- src/nested.ts
+++ src/nested.ts
@@ -1,12 +1,19 @@
 import { Answer } from "./interfaces/answer";
-import { Question, QuestionType } from "./interfaces/question";
+import { Question, QuestionType, createCopy } from "./interfaces/question";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    let newQuestions: Question[] = questions.reduce(
+        (accumulator: Question[], question: Question) => {
+            if (question.published) accumulator.push(createCopy(question));
+            return accumulator;
+        },
+        [],
+    );
+    return newQuestions;
 }
 
 /**
@@ -15,7 +22,21 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    let newQuestions: Question[] = questions.reduce(
+        (accumulator: Question[], question: Question) => {
+            if (
+                question.body !== "" &&
+                question.expected !== "" &&
+                question.options.length !== 0
+            ) {
+                let questionCopy: Question = createCopy(question);
+                accumulator.push(questionCopy);
+            }
+            return accumulator;
+        },
+        [],
+    );
+    return newQuestions;
 }
 
 /***
@@ -24,7 +45,7 @@ export function getNonEmptyQuestions(questions: Question[]): Question[] {
  */
 export function findQuestion(
     questions: Question[],
-    id: number
+    id: number,
 ): Question | null {
     return null;
 }
@@ -114,7 +135,7 @@ export function addNewQuestion(
     questions: Question[],
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question[] {
     return [];
 }
@@ -127,7 +148,7 @@ export function addNewQuestion(
 export function renameQuestionById(
     questions: Question[],
     targetId: number,
-    newName: string
+    newName: string,
 ): Question[] {
     return [];
 }
@@ -142,7 +163,7 @@ export function renameQuestionById(
 export function changeQuestionTypeById(
     questions: Question[],
     targetId: number,
-    newQuestionType: QuestionType
+    newQuestionType: QuestionType,
 ): Question[] {
     return [];
 }
@@ -161,7 +182,7 @@ export function editOption(
     questions: Question[],
     targetId: number,
     targetOptionIndex: number,
-    newOption: string
+    newOption: string,
 ): Question[] {
     return [];
 }
@@ -175,7 +196,7 @@ export function editOption(
 export function duplicateQuestionInArray(
     questions: Question[],
     targetId: number,
-    newId: number
+    newId: number,
 ): Question[] {
     return [];
 }
