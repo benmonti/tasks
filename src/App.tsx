eb087c1ddf8660a0dba6db4ff9d399420b597559
Ben Montimurro <benmonti@udel.edu>
2025-09-21 19:31:30 -0400

    Removed previous HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import doodlebob from "./images/doodlebob.png";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -12,23 +11,6 @@ import { Counter } from "./components/Counter";
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-                <br />
-                Ben Montimurro
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
-            </p>
-            <br />
-            <h1>This is doodlebob</h1>
-            <img src={doodlebob} alt="doodlebob" />
-            <ul className="App-list">
-                <li>Doodlebob is dangerous</li>
-                <li>Doodlebob was made by spongebob</li>
-                <li>Doodlebob uses a pencil to make things come to life</li>
-            </ul>
             <Button
                 onClick={() => {
                     console.log("Hello World!");
