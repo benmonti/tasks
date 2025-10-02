d7d6e059a11571948ef9fb1c78885ef27db26230
Ben Montimurro <benmonti@udel.edu>
2025-10-02 18:22:57 -0400

    Solved task 10: forms

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,44 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [remaining, setRemaining] = useState<number>(3);
+    const [requested, setRequested] = useState<string>("");
+
+    function increaseAttempts() {
+        const num = parseInt(requested);
+        if (!isNaN(num)) {
+            setRemaining(remaining + num);
+        }
+    }
+
+    function decreaseAttempts() {
+        setRemaining(remaining - 1);
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group
+                controlId="giveAttempts"
+                style={{ justifyItems: "center" }}
+            >
+                <Form.Label>Attempts Left:</Form.Label>
+                &nbsp;{remaining}
+                <br></br>
+                <Button onClick={decreaseAttempts} disabled={remaining === 0}>
+                    use
+                </Button>
+                <Button onClick={increaseAttempts}>gain</Button>
+                <Form.Control
+                    type="number"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setRequested(e.target.value);
+                    }}
+                    value={requested}
+                    style={{ width: "120px" }}
+                ></Form.Control>
+            </Form.Group>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,33 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+
+    function updateChoice(evt: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(evt.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group
+                controlId="multiple-choice-q"
+                style={{ justifyItems: "center" }}
+            >
+                <Form.Label>Choose an Answer From Selections: </Form.Label>
+                <Form.Select
+                    value={choice}
+                    onChange={updateChoice}
+                    style={{ width: "max-content" }}
+                >
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+                {choice === expectedAnswer ? "✔️" : "❌"}
+            </Form.Group>
         </div>
     );
 }
